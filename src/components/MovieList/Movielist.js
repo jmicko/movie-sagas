74574f1ee29db4916c697061e0f60779614505f0
John Micko <jrmicko5012@gmail.com>
2021-01-04 07:25:48 -0600

    map through movies and nav to movieitem component

--- src/components/MovieList/Movielist.js
+++ src/components/MovieList/Movielist.js
@@ -1,17 +1,38 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux';
 import MovieItem from '../MovieItem/MovieItem';
 
 
-export default class MovieList extends Component {
+class MovieList extends Component {
 
-    componentDidMount(){
+    componentDidMount() {
         this.props.dispatch({ type: 'FETCH_MOVIES' });
     }
+    
+    getMovieDetails = (movieId) => {
+        this.props.dispatch({ type: 'FETCH_MOVIE_DETAILS', payload: movieId });
+        this.props.history.push('/movie-details')
+    }
+
     render() {
         return (
             <div>
-                
+                {this.props.movies.map(movie =>
+                    <MovieItem
+                        key={movie.id}
+                        movie={movie}
+                        // passed through from app because it needs access to the App props
+                        getMovieDetails={this.getMovieDetails}
+                    />
+                )}
             </div>
         )
     }
 }
+
+
+const mapReduxStateToProps = reduxState => ({
+    reduxState
+});
+
+export default connect(mapReduxStateToProps)(MovieList);
